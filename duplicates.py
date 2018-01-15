97aca28d8a0557f05c085a3213ec2d792b01756d
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-15 14:13:15 +0300

    get_duplicates function added

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,27 @@
+import os
+import sys
+
+
+def get_duplicates(path_to_folder):
+    list_of_files = {}
+
+    for path, dirs, filenames in os.walk(path_to_folder):
+        for filename in filenames:
+            if (filename in list_of_files) and (
+                    os.path.getsize(os.path.join(path, filename)) ==
+                    os.path.getsize(list_of_files[filename])
+            ):
+                print(os.path.join(path, filename))
+                print(list_of_files[filename], end='\n\n')
+            else:
+                list_of_files[filename] = os.path.join(path, filename)
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        get_duplicates(sys.argv[1])
+    except IndexError:
+        print(
+            'Search path is not specified. '
+            'Use "python duplicates.py <path_to_file>"'
+        )
