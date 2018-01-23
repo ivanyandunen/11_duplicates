792a051eeb63db2c82e5dca6b71438cc74467d91
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-23 18:09:10 +0300

    folder existence check added. output fixed

--- duplicates.py
+++ duplicates.py
@@ -4,30 +4,33 @@ from collections import defaultdict
 
 
 def get_all_files(path_to_folder):
-    folder_content = defaultdict(list)
+    folder_content_dict = defaultdict(list)
     for path, dirs, filenames in os.walk(path_to_folder):
         for filename in filenames:
             filesize = os.path.getsize(os.path.join(path, filename))
-            folder_content[(filename, filesize)].append(
+            folder_content_dict[(filename, filesize)].append(
                 os.path.join(path, filename)
             )
-    return folder_content
+    return folder_content_dict
 
 
-def print_duplicates(folder_content):
-    for elem in folder_content:
-        if len(folder_content[elem]) > 1:
-            print('Files are duplicated: ')
-            print('\n'.join(folder_content[elem]))
+def print_duplicates(folder_content_dict):
+    for elem in folder_content_dict:
+        if len(folder_content_dict[elem]) > 1:
+            print('File {} is duplicated: '.format(elem[0]))
+            print('\n'.join(folder_content_dict[elem]))
             print('--------------------------------')
 
 
 if __name__ == '__main__':
     try:
-        folder_content = get_all_files(sys.argv[1])
-        print_duplicates(folder_content)
+        if not os.path.exists(sys.argv[1]):
+            print('Specified folder is not exist.')
     except IndexError:
         print(
             'Search path is not specified. '
             'Use "python duplicates.py <path_to_file>"'
         )
+    else:
+        folder_content_dict = get_all_files(sys.argv[1])
+        print_duplicates(folder_content_dict)
